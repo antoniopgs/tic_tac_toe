a422d884f671db4203d5ac6a5f52566a4a0053bf
António Gonçalves <apgoncalves@outlook.pt>
2019-05-09 22:08:42 +0100

    Revamp

---
+++ script.js
@@ -0,0 +1,19 @@
+let board = {
+    1: null,
+    2: null,
+    3: null,
+    4: null,
+    5: null,
+    6: null,
+    7: null,
+    8: null,
+    9: null
+};
+
+function play_1(spot) {
+    board[spot] = "X"
+};
+
+function play_2(spot) {
+    board[spot] = "O"
+};
